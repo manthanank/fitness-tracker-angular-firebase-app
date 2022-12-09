d32ff422b400d32855d9d244d67712b280d47edb
Manthan Ankolekar <manthan.ank@gmail.com>
2022-12-09 16:13:30 +0530

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBgKQglgoBzSEbmZN6P9FBSvlrplbKo7MI',
+    authDomain: 'angular-firebase-app-fb381.firebaseapp.com',
+    projectId: 'angular-firebase-app-fb381',
+    storageBucket: 'angular-firebase-app-fb381.appspot.com',
+    messagingSenderId: '285774140148',
+    appId: '1:285774140148:web:f2b0621e9ff054790644a1',
+    measurementId: 'G-4MWBKLLE2F',
+  },
+};
