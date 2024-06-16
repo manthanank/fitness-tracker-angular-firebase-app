599fada57e0273241f63fe1b4ff6ecf31e2ff0d0
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-06-16 18:54:57 +0530

    chore: Add user model and authentication data model

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: false,
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAgsK1Is0QwGwhCQsKAm5nsAcYxcez6vGU',
+    authDomain: 'ng-fitness-tracker.firebaseapp.com',
+    databaseURL: 'https://ng-fitness-tracker.firebaseio.com',
+    projectId: 'ng-fitness-tracker',
+    storageBucket: 'ng-fitness-tracker.appspot.com',
+    messagingSenderId: '183546960271'
+  }
 };
